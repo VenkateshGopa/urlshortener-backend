29c05522742e4fd3c193740d8129c50c8266f19f
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-12-24 11:53:09 +0530

    Update emailer.js

--- emailer/emailer.js
+++ emailer/emailer.js
@@ -1,6 +1,6 @@
 var nodemailer = require("nodemailer");
 
-const mailer = (email, code) => {
+const mailer = (email, code , sub) => {
   return new Promise((resolve, reject) => {
     var transporter = nodemailer.createTransport({
       service: "gmail",
@@ -13,7 +13,7 @@ const mailer = (email, code) => {
     var mailOptions = {
       from: "packatest@gmail.com",
       to: email,
-      subject: "Sending Email using Node.js",
+      subject:  `${sub}`,
       // text: 'That was easy!'
       html: `<p>${code} </p>`,
     };
